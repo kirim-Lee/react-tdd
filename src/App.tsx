655f119d9d5739f4b2c6f902c2e2739ff0e29916
이기림(kirim@sk.com) <kirim@sk.com>
2023-01-11 11:04:57 +0900

    이미지, 스타일 등 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import '../public/scss/style.scss';
+const logo = new URL('../public/images/logo.jpeg', import.meta.url);
 
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo.href} className="App-logo" alt="logo" />
+
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="app-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 };
 
 export default App;
