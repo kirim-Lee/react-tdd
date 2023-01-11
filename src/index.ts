9e3406fd58cfd8629f086f58f417b0887eeb7d6b
이기림(kirim@sk.com) <kirim@sk.com>
2023-01-11 10:27:11 +0900

    Initial commit - basic config

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('container');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
